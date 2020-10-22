90981c981300838d0502bab83ce22ebe14454a62
leonardogiannantoni <leonardo.giannantoni@gmail.com>
2020-10-22 05:17:49 +0200

    new example

--- main_simulation_example.py
+++ main_simulation_example.py
@@ -1,18 +1,24 @@
+from bacterial_consortium import bacterial_consortium
 from example_modules import *
 from simulator import *
 
-test_module = net1("test")
+#test_module = net1("test")
+test_module = bacterial_consortium("bacterial_consortium")
 #print(test_module)
 #TODO nella classe Module prevedere un meccanismo per l'enumerazione dei posti (con accesso solo a quelli
 # della rete superiore?) e aggiornamento del marking nei posti degli altri livelli connessi da "canali"
 marking = test_module.get_marking()
-marking['lower_net_a']['p1'].add([BlackToken()]*3)
-marking['lower_net_b']['e'].add([BlackToken()]*10)
-marking['upper_net']['p1'].add(["p1", "p1", "p1", "p2", "p2", "p2", "p2", "p2", "p2", "p2", "p2", "p2"])
-marking['upper_net']['p2'].add(["tk"]*10)
+marking['controller_net']['Lac1_protein'].add([BlackToken()]*50)
+marking['upper_net']['controller_compartment'].add(["Lac1_protein" for i in range(50)])
+marking['controller_net']['_3OC6HSL_gene'].add([BlackToken()]*1)
+marking['target_net']['GFP_gene'].add([BlackToken()]*1)
+#marking['controller_net']['_3OC6HSL_protein'].add([BlackToken()]*10)
+#marking['upper_net']['controller_compartment'].add(["_3OC6HSL_protein" for i in range(10)])
+#marking['target_net']['_3OC6HSL_protein'].add([BlackToken()]*10)
+#marking['upper_net']['target_compartment'].add(["_3OC6HSL_protein" for i in range(10)])
 test_module.set_marking(marking)
 
 output_path = os.path.join(".", test_module.name + "_results")
-s = Simulator(m=test_module, steps=100, output_path=output_path, draw_nets=False)
+s = Simulator(m=test_module, steps=500, output_path=output_path, draw_nets=False)
 s.execute()
 s.make_charts()
